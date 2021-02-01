479e5de99ff5fc7a95ba3f331ef2b61d52f9135e
Leystryku <Leystryku@gmail.com>
2021-02-01 15:00:52 +0100

    Remove unused diffclock fn

--- src/leysourceengineclient.cpp
+++ src/leysourceengineclient.cpp
@@ -165,17 +165,6 @@ int doreceivethink()
 	return 1;
 }
 
-
-bool needsforce = true;
-
-
-static long diffclock(clock_t clock1, clock_t clock2)
-{
-	clock_t diffticks = clock1 - clock2;
-	clock_t diffms = (diffticks) / (CLOCKS_PER_SEC / 1000);
-	return (long)diffms;
-}
-
 int dosendthink()
 {
 
